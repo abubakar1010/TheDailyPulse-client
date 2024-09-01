bcb53b0ab83b5fb2318808179b45e660b5407fdc
abu bakar <abubakar850772@gmail.com>
2024-09-01 22:47:41 +0600

    finished project setup with react router, tailwind and material tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button, Textarea } from "@material-tailwind/react"
+
+
+function App() {
+
+  return (
+    <>
+    <h1 className=" text-4xl bg-purple-700 py-12  " >Project Setup</h1>
+    <Button>TExt</Button>
+    <Textarea></Textarea>
+    </>
+  )
+}
+
+export default App
