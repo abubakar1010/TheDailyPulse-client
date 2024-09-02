3fa04bb734ca1e649e2806c14ded5203e071c6cd
abu bakar <abubakar850772@gmail.com>
2024-09-02 20:18:12 +0600

    solved dashboard drawer issues

--- src/Components/Drawer/DashboardDrawer.jsx
+++ src/Components/Drawer/DashboardDrawer.jsx
@@ -6,19 +6,17 @@ import {
     ListItem,
     ListItemPrefix,
   } from "@material-tailwind/react";
-import { TbBrandBooking } from "react-icons/tb";
-import { FaBook, FaCalendarAlt, FaShoppingBasket } from "react-icons/fa";
-import { FaCcAmazonPay, FaEnvelope, FaListUl, FaUsers, FaUtensils } from "react-icons/fa6";
-import { IoIosHome, IoMdCart } from "react-icons/io";
-import { MdOutlineMenuBook, MdOutlineRateReview } from "react-icons/md";
+import { FaBook, FaLeanpub } from "react-icons/fa";
+import {  FaUsers } from "react-icons/fa6";
+import { IoIosHome } from "react-icons/io";
 import { NavLink } from "react-router-dom";
-import useAdmin from "../../Hooks/useAdmin/useAdmin";
+// import useAdmin from "../../Hooks/useAdmin/useAdmin";
 // import { TbBrandBooking } from "react-icons/tb";
 
 const DashboardDrawer = () => {
 
 
-  const [isAdmin] = useAdmin()
+  // const [isAdmin] = useAdmin()
 
 
     return (
@@ -27,59 +25,18 @@ const DashboardDrawer = () => {
       <Drawer overlay={false} open={true} >
         <div className="mb-2 flex items-center justify-between p-4">
           <Typography variant="h5" color="blue-gray">
-            Material Tailwind
+            The Daily Pulse
           </Typography>
-          {/* <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </IconButton> */}
+          
         </div>
         <List>
 
-          {
-            isAdmin? <>
             <NavLink to={"/"}>
           <ListItem>
             <ListItemPrefix>
             <IoIosHome className=" text-xl" />
             </ListItemPrefix>
-            ADMIN HOME
-          </ListItem>
-          </NavLink>
-          <NavLink to={"/dashboard/addItems"}>
-          <ListItem>
-            <ListItemPrefix>
-            <FaUtensils className=" text-xl" />
-            </ListItemPrefix>
-            ADD ITEMS
-          </ListItem>
-          </NavLink>
-          <NavLink to={"/dashboard/manageItems"}>
-          <ListItem>
-            <ListItemPrefix>
-            <FaListUl className=" text-xl" />
-            </ListItemPrefix>
-            MANAGE ITEMS
-          </ListItem>
-          </NavLink>
-          <NavLink to={"/dashboard/manageBookings"}>
-          <ListItem>
-            <ListItemPrefix>
-            <FaBook className=" text-xl" />
-            </ListItemPrefix>
-            MANAGE BOOKINGS
+              HOME
           </ListItem>
           </NavLink>
           <NavLink to={"/dashboard/allUsers"}>
@@ -90,94 +47,25 @@ const DashboardDrawer = () => {
             ALL USER
           </ListItem>
           </NavLink>
-            </> : <>
-            {/* user Dashboard routes start here */}
-
-          <NavLink to={"/"}>
+          <NavLink to={"/dashboard/manageItems"}>
           <ListItem>
             <ListItemPrefix>
-            <IoIosHome className=" text-xl" />
+            <FaLeanpub className=" text-xl" />
             </ListItemPrefix>
-            USER HOME
+            ADD PUBLISHER
           </ListItem>
           </NavLink>
-          <NavLink to={"/dashboard/reservation"}>
+          <NavLink to={"/dashboard/allPublisher"}>
           <ListItem>
             <ListItemPrefix>
-            <FaCalendarAlt className=" text-xl" />
+            <FaBook className=" text-xl" />
             </ListItemPrefix>
-            RESERVATION
+            ALL ARTICLE
           </ListItem>
           </NavLink>
-          <NavLink to={"/dashboard/paymentHistory"}>
-          <ListItem>
-            <ListItemPrefix>
-            <FaCcAmazonPay className=" text-xl" />
-            </ListItemPrefix>
-            PAYMENT HISTORY
-          </ListItem>
-          </NavLink>
-          <NavLink to={"/dashboard/cart"}>
-          <ListItem>
-            <ListItemPrefix>
-            <IoMdCart className=" text-xl" />
-            </ListItemPrefix>
-            MY CART
-          </ListItem>
-          </NavLink>
-          <NavLink to={"/dashboard/addReview"}>
-          <ListItem>
-            <ListItemPrefix>
-            <MdOutlineRateReview className=" text-xl" />
-            </ListItemPrefix>
-            ADD REVIEW
-          </ListItem>
-          </NavLink>
-          <NavLink to={"/dashboard/myBooking"}>
-          <ListItem>
-            <ListItemPrefix>
-            <TbBrandBooking className=" text-xl" />
-            </ListItemPrefix>
-            MY BOOKING
-          </ListItem>
-          </NavLink>
+          
+            
 
-          {/* user Dashboard routes  end here */}
-            </>
-          }
-
-          <p className=" border-t-2 border-gray-700 w-[200px] my-4 "></p>
-
-          {/* common routes starts here */}
-
-        <NavLink to={"/"}>
-        <ListItem>
-            <ListItemPrefix>
-            <IoIosHome className=" text-xl" />
-            </ListItemPrefix>
-            HOME
-          </ListItem>
-        </NavLink>
-      <NavLink to={"/"}><ListItem>
-            <ListItemPrefix>
-            <FaEnvelope className=" text-xl" />
-            </ListItemPrefix>
-            CONTACT US
-          </ListItem></NavLink>
-      <NavLink to={"/menu"}><ListItem>
-            <ListItemPrefix>
-            <MdOutlineMenuBook className=" text-xl" />
-            </ListItemPrefix>
-            OUR MENU
-          </ListItem></NavLink>
-      <NavLink to={"/order/salad"}><ListItem>
-            <ListItemPrefix>
-            <FaShoppingBasket className=" text-xl" />
-            </ListItemPrefix>
-            ORDER FOOD
-          </ListItem></NavLink>
-
-          {/* common routes ed here */}
 
           
         </List>
