ccc6e626cefb36be7b20eb37b4c6c78c7e4671cf
abu bakar <abubakar850772@gmail.com>
2024-09-02 20:27:09 +0600

    solve login issues

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,46 +1,39 @@
 import {
     Card,
     Input,
-    Checkbox,
-    Button,
     Typography,
   } from "@material-tailwind/react";
-  import { useContext, useEffect, useRef, useState } from "react";
+  import { useState } from "react";
   import { Link, useLocation, useNavigate } from "react-router-dom";
-  import {
-    loadCaptchaEnginge,
-    LoadCanvasTemplate,
-    validateCaptcha,
-  } from "react-simple-captcha";
-  import { AuthContext } from "../../Provider/AuthProvider";
   import Swal from "sweetalert2";
+import { Helmet } from "react-helmet-async";
+import GoogleLogin from "../../Components/SocialLoign/GoogleLogin";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import useAuth from "../../Hooks/UseAuth/useAuth";
   
   const Login = () => {
   
-    const captchaRef = useRef(null)
-    const [isDisabled, setIsDisabled] = useState(true);
-    const {login} = useContext(AuthContext)
+    const {login} = useAuth()
     const navigate = useNavigate()
     const location = useLocation()
 
-
+    const [isError, setIsError] = useState(false);  
+    const [isShowPassword, setIsShowPassword] = useState(false);
+    
   
-    useEffect(() => {
-
-      loadCaptchaEnginge(6);
-
-    }, []);
+    const handleShowPassword = () => {
+      setIsShowPassword(!isShowPassword);
+    };
+  
   
     const handleLogin = (e) => {
-        
       e.preventDefault();
       const form = e.target;
   
       const email = form.email.value;
       const password = form.password.value;
   
-      const userInfo = { email, password };
-      console.log(userInfo);
+ 
       login(email, password)
       .then( () => {
   
@@ -54,7 +47,7 @@ import {
   
       })
       .catch(() => {
-        setIsDisabled(true)
+        setIsError(true)
         Swal.fire({
           icon: "error",
           title: "Oops...",
@@ -65,26 +58,32 @@ import {
   
     };
   
-    const handleCaptcha = () => {
-      const user_captcha_value = captchaRef.current.value
-      // console.log(user_captcha_value);
-      if (validateCaptcha(user_captcha_value)) {
-        setIsDisabled(false)
-    }
-      
-    }
   
   
     return (
       <>
-        <div className=" my-8 flex justify-center items-center">
-        <Card className="" color="transparent" shadow={false}>
+      <Helmet>
+        <title>Login | The Daily Pulse </title>
+      </Helmet>
+      <div className=" flex justify-center items-center my-16">
+        <Card color="transparent" shadow={false}>
           <Typography variant="h4" color="blue-gray">
-            Log in
+            Sign In
           </Typography>
-          <Typography color="gray" className="mt-1 font-normal">
-            Welcome Back! Enter your details to Login.
+          <Typography color="gray" className="mt-1 mb-7 font-normal">
+            Nice to meet you! Enter your details to Login.
           </Typography>
+
+          {isError && (
+            <>
+              <div className=" py-3 rounded-sm border border-[#FF5D64] text-center bg-[#ff5d651d] ">
+                <h1 className=" mb-1 text-lg font-bold text-[#121212e6]">
+                  Wrong credentials
+                </h1>
+                <p>Invalid username or password</p>
+              </div>
+            </>
+          )}
           <form
             onSubmit={handleLogin}
             className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
@@ -95,78 +94,63 @@ import {
               </Typography>
               <Input
                 size="lg"
+                type="email"
                 name="email"
-                
+                required
                 placeholder="name@mail.com"
                 className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
                 labelProps={{
                   className: "before:content-none after:content-none",
                 }}
               />
-              <Typography variant="h6" color="blue-gray" className="-mb-3">
-                Password
-              </Typography>
-              <Input
-                type="password"
-                name="password"
-                size="lg"
-                placeholder="********"
-                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                labelProps={{
-                  className: "before:content-none after:content-none",
-                }}
-              />
-              {
-                isDisabled? <><Typography variant="h6" color="blue-gray" className="-mb-3">
-                <LoadCanvasTemplate />
-              </Typography>
-              <input
-              ref={captchaRef}
-                type="text"
-                size="lg"
-                placeholder="Enter Captcha Value"
-                className=" border-gray-500 border py-3 rounded-md px-3 outline-none"
-                
-              />
-              <Button onClick={handleCaptcha}>Submit</Button></> : <p className=" text-lg my-2 text-center text-green-600">Matched</p>
-              }
-            </div>
-            <Checkbox
-              label={
-                <Typography
-                  variant="small"
-                  color="gray"
-                  className="flex items-center font-normal"
-                >
-                  I agree the
-                  <a
-                    href="#"
-                    className="font-medium transition-colors hover:text-gray-900"
-                  >
-                    &nbsp;Terms and Conditions
-                  </a>
+              <div className=" relative">
+                <Typography variant="h6" color="blue-gray" className="mb-3">
+                  Password
                 </Typography>
-              }
-              containerProps={{ className: "-ml-2.5" }}
-            />
-            <Button
-              disabled={isDisabled}
+                <Input
+                  type={isShowPassword ? "text" : "password"}
+                  name="password"
+                  required
+                  size="lg"
+                  placeholder="********"
+                  className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                />
+                <div
+                  onClick={handleShowPassword}
+                  className="absolute top-[50px] right-6"
+                >
+                  {isShowPassword ? (
+                    <FaEyeSlash className=" text-xl" />
+                  ) : (
+                    <FaEye className=" text-xl" />
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <button
               type="submit"
-              className="mt-6"
-              fullWidth
+              className="mt-6 bg-gradient-to-r from-[#ff0000] to-[#FF8938] w-full py-3 rounded-lg font-bold text-white text-xl"
             >
-              sign up
-            </Button>
+              sign In
+            </button>
             <Typography color="gray" className="mt-4 text-center font-normal">
-              {`Don't have an account?`}{" "}
-              <Link to={"/register"} className="font-medium text-gray-900">
-                Register
+              {`Don't have an account`}?{" "}
+              <Link
+                to={"/register"}
+                className="font-medium text-[#ff5d64ed] hover:underline"
+              >
+                Create an account
               </Link>
             </Typography>
+            <GoogleLogin />
           </form>
         </Card>
-        </div>
-      </>
+      </div>
+    </>
     );
   };
   
