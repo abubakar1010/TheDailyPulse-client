e0ac6812261a382399bd23a8d006a4920ef17807
abu bakar <abubakar850772@gmail.com>
2024-09-02 18:16:10 +0600

    finished added subscription page banner

---
+++ src/Components/SubscriptionBanner/SubscriptionBanner.jsx
@@ -0,0 +1,23 @@
+
+
+const SubscriptionBanner = () => {
+    return (
+        <>
+            <div className=" bg-[url('https://i.postimg.cc/Fz5Ryz1S/subscribe.jpg')] w-ful h-[520px] bg-cover bg-center bg-no-repeat my-8 rounded-xl relative ">
+            <div className="absolute inset-0 grid h-full w-full  items-center bg-[#1d1d1d70] rounded-xl">
+              <div className="text-white text-center pl-12">
+                <h1 className=" text-6xl font-extrabold ">Experience</h1>
+                <p className=" text-lg uppercase font-medium mt-5 mb-3">
+                  The New Standard Of Luxury With Every Purchase
+                </p>
+                <button className="  bg-gradient-to-r from-[#ff0000] to-[#FF8938] mt-8 py-2 px-5 font-bold text-lg rounded-md ">
+                  Explore Now
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+    );
+};
+
+export default SubscriptionBanner;
