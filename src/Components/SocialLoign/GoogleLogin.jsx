c0ea57951b7500cf3789b229b53e3c3004cfdcb3
abu bakar <abubakar850772@gmail.com>
2024-09-02 20:22:21 +0600

    finished solve google login bugs and save user in data base after successful login

--- src/Components/SocialLoign/GoogleLogin.jsx
+++ src/Components/SocialLoign/GoogleLogin.jsx
@@ -1,8 +1,7 @@
-import { Button } from "@material-tailwind/react";
-import { FaGoogle } from "react-icons/fa6";
 import useAuth from "../../Hooks/UseAuth/useAuth";
 import useAxiosPublic from "../../Hooks/useAxiosPublic/useAxiosPublic";
 import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 
 const GoogleLogin = () => {
@@ -15,7 +14,13 @@ const GoogleLogin = () => {
         googleLogin()
         .then( res => {
             console.log(res);
-            const userInfo = { email: res.user?.email, name: res.user?.displayName}
+            const userInfo = { 
+              name: res.user?.displayName,
+              email: res.user?.email,
+              image: res.user?.photoURL,
+              lastLogin: res.user?.metadata.lastSignInTime,
+              role: "user",
+            }
             axiosPublic.post("/users", userInfo)
             .then( (res) => {
                 console.log(res);
@@ -28,12 +33,24 @@ const GoogleLogin = () => {
         })
     }
     return (
-        <div>
-            <Button onClick={handleGoogleLogin} className=" flex gap-2 justify-center items-center">
-                <FaGoogle />
-                Google
-            </Button>
-        </div>
+        <>
+        <div className="inline-flex items-center justify-center w-full mt-2">
+              <hr className="w-full h-px my-8 bg-[#AAAAAA] border-0 " />
+              <span className="absolute px-3 font-medium text-xl -translate-x-1/2 bg-white left-1/2 ">
+                or
+              </span>
+            </div>
+
+            <div
+              onClick={handleGoogleLogin}
+              className=" flex gap-4 border-2 border-[#4c4a4aae] items-center py-3 rounded-full justify-center cursor-pointer"
+            >
+              <FcGoogle className=" text-3xl" />
+              <p className=" text-lg font-medium text-[#151515ca]">
+                Continue with Google
+              </p>
+            </div>
+        </>
     );
 };
 
