a975da52425eb197c9e4fba66e4f9c1f69aa9e37
abu bakar <abubakar850772@gmail.com>
2024-09-02 17:03:33 +0600

    finished create use news hook for fetch data

---
+++ src/Hooks/UseNews/useNews.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useNews = () => {
+
+    const [news, setNews] = useState([]);
+    const [loader, setLoader] = useState(true)
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/news')
+    .then( res => {
+      setNews(res.data)
+      setLoader(false)
+    })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return [news, loader]
+
+}
+
+export default useNews;
